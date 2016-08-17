2a4f9080fb5d48a94b65a5a58dc9fb47ddcb657c
Adrian <a.louis@zeppelin-university.net>
2016-08-17 14:59:31 +0200

    5.4outa

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,3 +6,12 @@ class StaticPagesController < ApplicationController
 
   end
 end
+def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'your-email@example.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "a.louis@zeppelin-university.net"
   layout 'mailer'
 end
