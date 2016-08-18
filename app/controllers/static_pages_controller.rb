3d541e21c3a7ae2aaa2587eaef4537bb33bb39a1
Adrian <a.louis@zeppelin-university.net>
2016-08-18 10:54:36 +0200

    Maily

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,17 +1,16 @@
 class StaticPagesController < ApplicationController
-	
+def index 
+end
 	def landing_page
   	@featured_product = Product.first
     @products = Product.limit(3)
-
-  end
 end
-def thank_you
+
+    def thank_you
   @name = params[:name]
   @email = params[:email]
   @message = params[:message]
-  ActionMailer::Base.mail(:from => @email,
-      :to => 'your-email@example.com',
-      :subject => "A new contact form message from #{@name}",
-      :body => @message).deliver_now
-end
+  UserMailer.contact_form(@email, @name, @message).deliver_now
+  
+	end
+  end
