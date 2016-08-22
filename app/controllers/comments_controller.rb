8a1fa45d2c2c108a212459f376ef0d5853de46f1
Adrian <a.louis@zeppelin-university.net>
2016-08-22 14:45:28 +0200

    Rate it 5.8

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+	class CommentsController < ApplicationController
+
+def create
+    @product = Product.find(params[:product_id])
+    @comment = @product.comments.new(comment_params)
+    @comment.user = current_user
+    @comment.save
+    redirect_to product_path(@product)
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    product = @comment.product
+    @comment.destroy
+    redirect_to product
+  end
+end
+
+private
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :body, :rating)
+  end
