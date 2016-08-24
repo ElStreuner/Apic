e114ec75cfd87b1418ac542d132fcd6bdfe1aa5a
Adrian <a.louis@zeppelin-university.net>
2016-08-24 12:09:02 +0200

    5.10 improved

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,8 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
+  before_action :authenticate_user!, :except => [:show, :index]
+  load_and_authorize_resource
 
   # GET /products
   # GET /products.json
