41a30c15d547d97f84d98aa7cf4cacac80229a89
Adrian <a.louis@zeppelin-university.net>
2016-09-15 15:01:10 +0200

    6.13 style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,12 @@
 //= require turbolinks
 //= require_tree .
 //= require chosen-jquery
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+  }
+}
--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -11,3 +11,13 @@ $(document).on('turbolinks:load', function() {
     refreshRating();
     $('.img-zoom').elevateZoom();
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
+}
