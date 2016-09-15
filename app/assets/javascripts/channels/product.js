3348a41479bb4a2b7ef4aeccc21f5716a86d2fef
Adrian <a.louis@zeppelin-university.net>
2016-09-15 16:32:02 +0200

    6.14 morrison

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -1,6 +1,7 @@
 App.product = App.cable.subscriptions.create({channel: "ProductChannel"}, {
   connected: function() {
     // Called when the subscription is ready for use on the server
+
   },
 
   disconnected: function() {
@@ -10,5 +11,18 @@ App.product = App.cable.subscriptions.create({channel: "ProductChannel"}, {
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
     $(".alert.alert-info").show();
+    $('.product-reviews').prepend(data.comment);
+    refreshRating();
+  },
+
+  listen_to_comments: function() {
+    return this.perform('listen', {
+      product_id: $("[data-product-id]").data("product-id")
+    });
   }
+
+});
+
+$(document).on('turbolinks:load', function() {
+  App.product.listen_to_comments();
 });
